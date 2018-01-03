e913f00ed6b9c4c9cc3cdf9614767a1fcd672afb
Kenichi Saito <hajime@studiohff.net>
2018-01-04 08:38:28 +0900

    DIコンテナの初期化が終わってからElectronの起動処理を行うように変更

--- src/main/main.ts
+++ src/main/main.ts
@@ -17,107 +17,119 @@ Commander
     .option("--use-stub")
     .parse(process.argv);
 
-let window: BrowserWindow | null;
+async function initContainer() {
+    const container = new inversify.Container();
 
-function createWindow() {
-    window = new BrowserWindow({ width: 800, height: 600 });
+    if (Commander.useStub) {
+        container.bind<ITwitterClient>(TYPES.TwitterClient).to(StubTwitterClient);
+    }
+    else {
+        container.bind<ITwitterClient>(TYPES.TwitterClient).toConstantValue(new Twitter({
+            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        }));
+    }
+    container.bind<TwitterGateway>(TYPES.TwitterGateway).to(TwitterGateway);
 
-    window.loadURL(url.format({
-        pathname: path.join(__dirname, "../../static/index.html"),
-        protocol: "file:",
-        slashes: true
-    }));
+    return container;
+}
 
-    window.webContents.openDevTools();
+function initElectron() {
+    let window: BrowserWindow | null;
 
-    window.on("closed", () => {
-       window = null;
+    function createWindow() {
+        window = new BrowserWindow({ width: 800, height: 600 });
+
+        window.loadURL(url.format({
+            pathname: path.join(__dirname, "../../static/index.html"),
+            protocol: "file:",
+            slashes: true
+        }));
+
+        window.webContents.openDevTools();
+
+        window.on("closed", () => {
+        window = null;
+        });
+    }
+
+    app.on("ready", createWindow);
+
+    app.on("window-all-closed", () => {
+        if (process.platform !== "darwin") {
+            app.quit();
+        }
+    });
+
+    app.on("activate", () => {
+        if (window === null) {
+            createWindow();
+        }
+    });
+
+    ipcMain.on(ipcMessage.analyze, (event, screenName) => {
+        event.sender.send(ipcMessage.analyzeStart);
+
+        setTimeout(() => {
+            event.sender.send(ipcMessage.analyzeProgress, `Analyzing (1/3) ...`);
+        }, 1000);
+
+        setTimeout(() => {
+            event.sender.send(ipcMessage.analyzeProgress, `Analyzing (2/3) ...`);
+        }, 2000);
+
+        setTimeout(() => {
+            event.sender.send(ipcMessage.analyzeProgress, `Analyzing (3/3) ...`);
+        }, 3000);
+
+        setTimeout(() => {
+            event.sender.send(ipcMessage.analyzeProgress, `Analyzing finish!!`);
+        }, 4000);
+
+        setTimeout(() => {
+            /* tslint:disable:object-literal-sort-keys */
+            const dummyData = {
+                screenName: "hajimepg",
+                profileImageUrl: "./hajimepg.jpg",
+                twitterHomeUrl: "https://twitter.com/hajimepg",
+            };
+
+            const result = {
+                followEachOther: [dummyData],
+                followedOnly: [dummyData, dummyData],
+                followOnly: [dummyData, dummyData, dummyData]
+            };
+            /* tslint:enable:object-literal-sort-keys */
+
+            event.sender.send(ipcMessage.analyzeFinish, result);
+        }, 4500);
     });
 }
 
-app.on("ready", createWindow);
+initContainer().then((container) => {
+    const twitterGateway = container.get<TwitterGateway>(TYPES.TwitterGateway);
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-});
-
-app.on("activate", () => {
-    if (window === null) {
-        createWindow();
-    }
-});
-
-ipcMain.on(ipcMessage.analyze, (event, screenName) => {
-    event.sender.send(ipcMessage.analyzeStart);
-
-    setTimeout(() => {
-        event.sender.send(ipcMessage.analyzeProgress, `Analyzing (1/3) ...`);
-    }, 1000);
-
-    setTimeout(() => {
-        event.sender.send(ipcMessage.analyzeProgress, `Analyzing (2/3) ...`);
-    }, 2000);
-
-    setTimeout(() => {
-        event.sender.send(ipcMessage.analyzeProgress, `Analyzing (3/3) ...`);
-    }, 3000);
-
-    setTimeout(() => {
-        event.sender.send(ipcMessage.analyzeProgress, `Analyzing finish!!`);
-    }, 4000);
-
-    setTimeout(() => {
-        /* tslint:disable:object-literal-sort-keys */
-        const dummyData = {
-            screenName: "hajimepg",
-            profileImageUrl: "./hajimepg.jpg",
-            twitterHomeUrl: "https://twitter.com/hajimepg",
-        };
-
-        const result = {
-            followEachOther: [dummyData],
-            followedOnly: [dummyData, dummyData],
-            followOnly: [dummyData, dummyData, dummyData]
-        };
-        /* tslint:enable:object-literal-sort-keys */
-
-        event.sender.send(ipcMessage.analyzeFinish, result);
-    }, 4500);
-});
-
-const container = new inversify.Container();
-
-if (Commander.useStub) {
-    container.bind<ITwitterClient>(TYPES.TwitterClient).to(StubTwitterClient);
-}
-else {
-    container.bind<ITwitterClient>(TYPES.TwitterClient).toConstantValue(new Twitter({
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    }));
-}
-container.bind<TwitterGateway>(TYPES.TwitterGateway).to(TwitterGateway);
-
-const twitterGateway = container.get<TwitterGateway>(TYPES.TwitterGateway);
-
-twitterGateway.on("onRequest", (endpoint, options) => {
-    console.log(`onRequest endpoint=${endpoint}, options=${options}`);
-});
-twitterGateway.on("onRateLimit", (endpoint, options) => {
-    console.log(`onRateLimit endpoint=${endpoint}, options=${options}`);
-});
-twitterGateway.on("onRequestSuccuess", (endpoint, options, response) => {
-    console.log(`onRequestSuccuess endpoint=${endpoint}, options=${options} responses.length=${response.length}`);
-});
-twitterGateway.on("onComplete", (endpoint, options, responses) => {
-    console.log(`onComplete endpoint=${endpoint}, options=${options} responses.length=${responses.length}`);
-});
-
-twitterGateway.getFollowers("hajimepg")
-    .then((followers) => {
-        // console.log(JSON.stringify(followers, null, 4));
+    twitterGateway.on("onRequest", (endpoint, options) => {
+        console.log(`onRequest endpoint=${endpoint}, options=${options}`);
     });
+    twitterGateway.on("onRateLimit", (endpoint, options) => {
+        console.log(`onRateLimit endpoint=${endpoint}, options=${options}`);
+    });
+    twitterGateway.on("onRequestSuccuess", (endpoint, options, response) => {
+        console.log(`onRequestSuccuess endpoint=${endpoint}, options=${options} responses.length=${response.length}`);
+    });
+    twitterGateway.on("onComplete", (endpoint, options, responses) => {
+        console.log(`onComplete endpoint=${endpoint}, options=${options} responses.length=${responses.length}`);
+    });
+
+    twitterGateway.getFollowers("hajimepg")
+        .then((followers) => {
+            // console.log(JSON.stringify(followers, null, 4));
+        });
+
+    initElectron();
+}).catch((error) => {
+    console.log(error);
+});
